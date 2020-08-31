c0c4d580f201888f49b24115f9a6ab60e8568d59
VAMSI MEDABALIMI <54467427+vamsi-415@users.noreply.github.com>
2020-08-31 13:05:41 +0530

    Add files via upload
    simple if program

---
+++ simpleif.java
@@ -0,0 +1,18 @@
+/*
+	multi line comments
+	This is a demo for simple if
+*/
+
+
+//single line comment
+import java.io.*;
+public class simpleif{
+	public static void main(String args[]){
+		int x=20;
+		System.out.println("program started");
+		if ( x == 20 ){
+			System.out.println("Hi");
+		}
+		System.out.println("The end");
+	}
+}		
